4b44b32f898f72b86f10d5d868fc12e314c23722
pagodingo <daniel.danielarchibald@gmail.com>
2022-07-22 16:48:23 -0400

    -

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends React.Component{
                                                   toolKit.Images.loadEmbeddedImages(archive)
     })
 
-    this.setState({page: nextPage})
+    this.setState({currentPage: nextPage})
   }
 
   render(){
