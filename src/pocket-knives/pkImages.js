be2b002a5c54e55adf494e257120c0a93d92d3af
pagodingo <daniel.danielarchibald@gmail.com>
2022-07-26 19:56:09 -0400

    -

--- src/pocket-knives/pkImages.js
+++ src/pocket-knives/pkImages.js
@@ -1,15 +1,17 @@
 const pkImages = {
-    loadEmbeddedImages: function(source){
-        let images = document.getElementsByTagName("img");
-        let array = [].slice.call(images);
+    loadEmbeddedImages: loadEmbeddedImages,
+}
 
-        array.forEach((image) => {
-            let slug = image.src.split('/').filter(e => e !== '')
-            console.log(slug)
-            let png = slug[slug.length - 1]
-                image.src = `https://raw.githubusercontent.com/${source}/master/images/${png}`
-        });
-    },
+function loadEmbeddedImages(source){
+    let images = document.getElementsByTagName("img");
+    let array = [].slice.call(images);
+
+    array.forEach((image) => {
+        let slug = image.src.split('/').filter(e => e !== '')
+        //console.log(slug)
+        let png = slug[slug.length - 1]
+            image.src = `https://raw.githubusercontent.com/${source}/master/images/${png}`
+    });
 }
 
 export default pkImages
--- src/pocket-knives/pkMarkdown.js
+++ src/pocket-knives/pkMarkdown.js
@@ -1,18 +1,22 @@
 const pkMarkdown = {
-    cleanBeforeRender: function(html){
-       return html
-       .replace(/&lt;/g, "<")
-       .replace(/&gt;/g, ">")
-       .replace(/&quot;/g, "")
-    },
-    returnMarkdownFiles: function(source){
-        let files = []
-        source.forEach(item => {
-          if (item.name.endsWith('.md')){
-            files.push(item.name)
-          }})
-        return files
-    }
+    cleanBeforeRender: cleanBeforeRender,
+    returnMarkdownFiles: returnMarkdownFiles
+}
+
+function returnMarkdownFiles(source){
+  let files = []
+  source.forEach(item => {
+    if (item.name.endsWith('.md')){
+      files.push(item.name)
+    }})
+  return files
+}
+
+function cleanBeforeRender(html){
+  return html
+  .replace(/&lt;/g, "<")
+  .replace(/&gt;/g, ">")
+  .replace(/&quot;/g, "")
 }
 
 export default pkMarkdown
