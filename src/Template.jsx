1278598856b2c1c2bc3d090aad016cb236e6f712
pagodingo <daniel.danielarchibald@gmail.com>
2022-07-29 03:26:23 -0400

    -

--- src/Template.jsx
+++ src/Template.jsx
@@ -3,8 +3,8 @@ const Template = ({ currentPage, contents, nextPage, searchChange}) => {
     <>
       <div id="left">
         
-        <p style={{ textAlign: "left" }}>Contents</p>
-        <input type="text" placeholder="..." onChange={searchChange} style={{top: -10, position: 'sticky', background: 'white', borderTop: '10px solid white', width: '100%'}}></input>
+        <p style={{ textAlign: "left"}}>Contents</p>
+        <input type="text" placeholder="..." onChange={searchChange} style={{top: -11.5, position: 'sticky', background: 'white', borderTop: '10px solid white', width: '100%'}}></input>
         {contents.map((content, i) => {
           return (
             <div id="contents" key={i}>
@@ -17,7 +17,7 @@ const Template = ({ currentPage, contents, nextPage, searchChange}) => {
       </div>
 
       <div id="right">
-        <p id="page-title" style={{textDecoration: "underline"}}>{currentPage}</p>
+        <p id="page-title" style={{textDecoration: "underline", borderBottom: "1px solid #ddd"}}>{currentPage}</p>
         <div id="main" />
       </div>
     </>
