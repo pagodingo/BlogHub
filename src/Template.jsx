6e6152b21b6ec0f9111bac577ac50a1529fb836f
pagodingo <daniel.danielarchibald@gmail.com>
2022-08-01 11:03:15 -0400

    -

--- src/Template.jsx
+++ src/Template.jsx
@@ -1,4 +1,9 @@
+const archive = process.env.REACT_APP_GIT_USER_REPO
 const Template = ({ currentPage, contents, nextPage, searchChange}) => {
+  const editOnGithubHandler = () => {
+    window.open(
+    `https://www.github.com/${archive}/edit/master/${currentPage}`, "_blank");
+  }
   return (
     <>
       <div id="left">
@@ -26,6 +31,7 @@ const Template = ({ currentPage, contents, nextPage, searchChange}) => {
               transform: 'skewY(-40deg)',
               }}></div>
         <p id="page-title" style={{textDecoration: "underline", borderBottom: "1px solid #ddd"}}>{currentPage}</p>
+        <p style={{textAlign: 'right', position: 'fixed', right: '15px', top: '0', margin: '10px', cursor: 'pointer'}} onClick={editOnGithubHandler}><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" style={{width: '23px', height: 'auto', position: 'fixed', right: '120px', top: 1}} />Edit on GitHub</p>
         <div id="main" />
       </div>
       </>
