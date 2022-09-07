42ceda266a67cbf986aed2438557b9f1b00b193d
pagodingo <daniel.danielarchibald@gmail.com>
2022-09-07 18:27:38 -0400

    -

--- src/js/jsMarkdown.js
+++ src/js/jsMarkdown.js
@@ -1,26 +1,23 @@
-function jsMarkdown() {
-  this.cleanBeforeRender = cleanBeforeRender
-  this.returnMarkdownFiles = returnMarkdownFiles
+function jsMarkdown() {};
 
-  function returnMarkdownFiles(source){
-    let files = []
-    source.forEach(item => {
-      if (item.name.endsWith('.md')){
-        files.push(`${item.name}`)
-      } else if (item.type === "dir") {
-        if (item.name !== "images"){
-          files.push(`📚 ${item.name}`)
-        }
-    }})
-    return files
-  }
+jsMarkdown.prototype.returnMarkdownFiles = (source) => {
+  let files = []
+  source.forEach(item => {
+    if (item.name.endsWith('.md')){
+      files.push(`${item.name}`)
+    } else if (item.type === "dir") {
+      if (item.name !== "images"){
+        files.push(`📚 ${item.name}`)
+      }
+  }})
+  return files
+}
 
-  function cleanBeforeRender(html){
-    return html
-    .replace(/&lt;/g, "<")
-    .replace(/&gt;/g, ">")
-    .replace(/&quot;/g, "")
-  }
+jsMarkdown.prototype.cleanBeforeRender = (html) => {
+  return html
+  .replace(/&lt;/g, "<")
+  .replace(/&gt;/g, ">")
+  .replace(/&quot;/g, "")
 }
 
 export default jsMarkdown
